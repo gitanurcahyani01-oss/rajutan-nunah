390a137e2fb8199375752bb1dbf909ebffb7aaf7
Gita Nurcahyani <gitanurcahyani01@gmail.com>
2026-05-07 21:21:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -68,9 +68,9 @@ customOrderBtn.addEventListener('click', () => {
   let message;
 
   if (idea) {
-    message = `Halo Nunah! 👋 Saya ingin membuat Custom Order 🧶\n\nIde saya:\n${idea}\n\nBisa bantu saya?`;
+    message = `Halo Nunah!, Saya ingin membuat Custom Order \n\nIde saya:\n${idea}\n\nBisa bantu saya?`;
   } else {
-    message = `Halo Nunah! 👋 Saya ingin membuat Custom Order 🧶\n\nBoleh saya konsultasi dulu?`;
+    message = `Halo Nunah!, Saya ingin membuat Custom Order \n\nBoleh saya konsultasi dulu?`;
   }
 
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
